3c89c4ba7746938216c5b43bb3043724487b812f
TousifNuhan <mubtasimtousif@gmail.com>
2024-02-17 00:16:00 +0600

    Completed rectangle area calculator

---
+++ rectangle-calculator.js
@@ -0,0 +1,14 @@
+function rectangleArea() {
+    const widthInput = document.getElementById('width').value;
+    const width = parseFloat(widthInput);
+    // console.log(width);
+
+    const lenthInput = document.getElementById('length').value;
+    const length = parseFloat(lenthInput);
+    // console.log(length);
+
+    document.getElementById('width').value = '';
+
+    const area = width * length;
+    console.log(area);
+}
